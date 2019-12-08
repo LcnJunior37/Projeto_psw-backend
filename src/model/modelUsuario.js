426c6b4a33578fbd7281eb8c69d853a20ded069b
lcnjunior37 <lcnjunior37@gmail.com>
2019-12-08 16:26:40 -0300

    code refactoring

--- src/model/modelUsuario.js
+++ src/model/modelUsuario.js
@@ -1,14 +1,14 @@
 "user strict";
-var sql = require("./db.js");
+var sql = require("../model/dbConnection.js");
 
 var user = function(user) {
   this.codUsuario = user.codUsuario;
   this.tipo = user.tipo;
-  this.nick = user.nick;
-  this.nick = user.senha;
+  this.nome = user.nome;
+  this.senha = user.senha;
 };
 User.createUser = function(newUser, result) {
-  sql.query("INSERT INTO Users set ?", newUser, function(err, res) {
+  sql.query("INSERT INTO usuario set ?", newUser, function(err, res) {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -19,16 +19,20 @@ User.createUser = function(newUser, result) {
   });
 };
 User.getUserById = function(UserId, result) {
-  sql.query("Select User from Users where id = ? ", UserId, function(err, res) {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      result(null, res);
+  sql.query(
+    "Select codUsuario,tipo,nome from usuario where codUsuario = ? ",
+    UserId,
+    function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
     }
-  });
+  );
 };
-User.getAllUser = function(result) {
+/*User.getAllUser = function(result) {
   sql.query("Select * from Users", function(err, res) {
     if (err) {
       console.log("error: ", err);
@@ -39,11 +43,11 @@ User.getAllUser = function(result) {
       result(null, res);
     }
   });
-};
-User.updateById = function(id, User, result) {
+};*/
+User.updateById = function(User, result) {
   sql.query(
-    "UPDATE Users SET User = ? WHERE id = ?",
-    [User.codUsuario, id],
+    "UPDATE usuario SET tipo = ?, nome = ? WHERE codUsuario = ?",
+    [User.tipo, User.nome, User.codUsuario],
     function(err, res) {
       if (err) {
         console.log("error: ", err);
@@ -55,7 +59,10 @@ User.updateById = function(id, User, result) {
   );
 };
 User.remove = function(id, result) {
-  sql.query("DELETE FROM Users WHERE id = ?", [id], function(err, res) {
+  sql.query("DELETE FROM usuario WHERE codUsuario= ?", [id], function(
+    err,
+    res
+  ) {
     if (err) {
       console.log("error: ", err);
       result(null, err);
