ae6bb8066bb8694a542deec4b557d765c473de86
Luciano Moreira dos Santos Junior <Luciano.Moreira@ibm.com>
2019-12-12 15:26:15 -0300

    vehicle.repository completed

--- src/repositories/vehicle.repository.js
+++ src/repositories/vehicle.repository.js
@@ -66,6 +66,50 @@ const create = vehicle => {
         }
     });
 }
+const updateOne = (id, dataToUpdate) => {
+    const keys = Object.keys(dataToUpdate);
+    const values = Object.values(dataToUpdate);
+  
+    const UPDATE = `UPDATE veiculo SET `;
+  
+    let SET = '';
+    keys.forEach((r, index) => {
+      SET+=`${r} = \'${values[index]}\', `;
+    })
+  
+    SET = SET.replace(/, $/, ' ');
+    
+    const WHERE = `WHERE codVeiculo = ${id}`;
+  
+    const sqlQuery = UPDATE + SET + WHERE;;
+  
+    return new Promise((resolve, reject) => {
+      databaseService.query(
+        sqlQuery,
+        (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(dataToUpdate);
+        }
+      );
+    });
+  };
+
+  const deleteOne = async id => {
+    return new Promise((resolve, reject) => {
+      databaseService.query(
+        `DELETE FROM veiculo  WHERE codVeiculo = ${id}`,
+        (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(id);
+        }
+      );
+    });
+  };
+  
 module.exports = {
     findAll,
     findById,
