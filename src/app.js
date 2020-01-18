893f370549bf72b011acb6b787e66368b06112ae
lcnjunior37 <lcnjunior37@gmail.com>
2020-01-18 19:34:18 -0300

    Refactoring

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
 const routesUser = require("./routes/user.router");
-const bodyParser = require('body-parser');
+const routesVehicle = require("./routes/vehicle.router");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || "3000";
 const host = process.env.PORT || "localhost";
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(
   bodyParser.json({
-    limit: '50mb'
+    limit: "50mb"
   })
 );
 app.use(
   bodyParser.urlencoded({
-    limit: '50mb',
+    limit: "50mb",
     extended: true
   })
 );
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
-app.use('/', routesUser);
+app.use("/user", routesUser);
+app.use("/vehicle", routesVehicle);
 
 app.use((req, res, next) => {
   res.status(404);
--- src/services/vehicle.service.js
+++ src/services/vehicle.service.js
@@ -1,20 +1,8 @@
-const userRepository = require('../repositories/vehicle.repository');
-const bcrypt = require('bcrypt');
+const vehicleRepository = require("../repositories/vehicle.repository");
 
-const findAllUsers = async (req, res) => {
+const findAllVehicle = async (req, res) => {
   try {
-    const result = await userRepository.findAll();
-    res.send(result);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-}
-
-const findUserById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    let result = await userRepository.findById(id);
+    const result = await vehicleRepository.findAll();
     res.send(result);
   } catch (err) {
     console.error(err);
@@ -22,7 +10,18 @@ const findUserById = async (req, res) => {
   }
 };
 
-const createUser = async (req, res) => {
+const findVehicleById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let result = await vehicleRepository.findById(id);
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+};
+
+const createVehicle = async (req, res) => {
   const requestBody = req.body;
   if (
     requestBody.codVeiculo &&
@@ -32,17 +31,17 @@ const createUser = async (req, res) => {
     requestBody.tipo
   ) {
     try {
-      const senhaEncriptada = encryptPassword(req.body.senha);
+      // const senhaEncriptada = encryptPassword(req.body.senha);
 
       const veh = {
-        codVeiculo = vehicle.codVeiculo,
-        placa = vehicle.placa,
-        modelo = vehicle.modelo,
-        ano = vehicle.ano,
-        tipo = vehicle.tipo
+        codVeiculo: req.body.codVeiculo,
+        placa: req.body.placa,
+        modelo: req.body.modelo,
+        ano: req.body.ano,
+        tipo: req.body.tipo
       };
 
-      const result = await userRepository.create(veh);
+      const result = await vehicleRepository.create(veh);
       res.send(result);
     } catch (err) {
       console.error(err);
@@ -50,56 +49,53 @@ const createUser = async (req, res) => {
     }
   } else {
     res.status(400).send({
-      error: 'Campos Faltando'
+      error: "Campos Faltando"
     });
   }
 };
 
-const updateUser = async (req, res) => {
+const updateVehicle = async (req, res) => {
   const requestBody = req.body;
-    if (
-        requestBody.codVeiculo ||
-        requestBody.placa ||
-        requestBody.modelo ||
-        requestBody.ano ||
-        requestBody.tipo
-    ) {
-      try {
-        
-        const id = req.params.id;
-        const dataToUpdate = requestBody;
-        await userRepository.updateOne(id, dataToUpdate);
+  if (
+    requestBody.codVeiculo ||
+    requestBody.placa ||
+    requestBody.modelo ||
+    requestBody.ano ||
+    requestBody.tipo
+  ) {
+    try {
+      const id = req.params.id;
+      const dataToUpdate = requestBody;
+      await vehicleRepository.updateOne(id, dataToUpdate);
 
-        res.status(200).send(dataToUpdate);
-      } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-      }
-    } else {
-      res.status(400).send({
-        error: 'Campos Faltando'
-      });
+      res.status(200).send(dataToUpdate);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
     }
+  } else {
+    res.status(400).send({
+      error: "Campos Faltando"
+    });
+  }
 };
 
-const deleteUser = async (req, res) => {
+const deleteVehicle = async (req, res) => {
   try {
     const id = req.params.id;
     await userRepository.deleteOne(id);
 
     res.send({ msg: `Veiculo com id ${id} deletado.` });
   } catch (err) {
-     console.error(err);
-     res.sendStatus(500);
+    console.error(err);
+    res.sendStatus(500);
   }
 };
 
-
-
-module.exports ={
-  findAllUsers,
-  findUserById,
-  createUser,
-  updateUser,
-  deleteUser
-}
+module.exports = {
+  findAllVehicle,
+  findVehicleById,
+  createVehicle,
+  updateVehicle,
+  deleteVehicle
+};
